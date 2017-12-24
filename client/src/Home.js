3504a3fca3eab2e67b2f2187f3f1ac2d5c2b4af1
Ethan Arrowood <ethan.arrowood@gmail.com>
2017-12-24 03:11:58 -0500

    built rough home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -2,9 +2,14 @@ import React from 'react';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 
 import './Home.css';
+import Cookie from './cookie.svg';
 
 export default ({ apiStatus }) => (
-  <div className="abc">
+  <div className="container" style={{
+    height: "100vh",
+    display: "grid",
+    gridTemplate: "80px auto 70px / auto",
+  }}>
     <div className="header">
       <div className="title">
         <h1>Cookie Jar</h1>
@@ -18,7 +23,14 @@ export default ({ apiStatus }) => (
       </ul>
     </div>
     <div className="body">
-      <p>Cookie Jar Body</p>
+      <div className="cookie-section">
+        <img src={Cookie} className="cookie" alt="cookie" />
+      </div>
+      <div className="text-section">
+        <h1>Cookie Jar - Incentivised Task Management</h1>
+        <p>With Cookie Jar, you can better manage your team by rewarding them for every task they complete. Each team member or "Cookie Collector" gets a "Cookie Jar" that tracks how many cookies they have earned. The manager can create and edit tasks, assigning cookie values to them. When another user completes that task and it is approved by the manager, the incentive is added to that collectors cookie jar!</p>
+        <button className="get-started">Get Started</button>
+      </div>
     </div>
     <div className="footer">
       <div className="social-media">
@@ -29,11 +41,10 @@ export default ({ apiStatus }) => (
         </ul>
       </div>
       <div className="api-status">
-        
         <span>API Status: <span style={{color: apiStatus === 'OK' ? 'green' : 'red'}}>{apiStatus || ''}</span></span>
       </div>
-      <div className="copywrite">
-        <span>Copywrite 2017 Ethan Arrowood</span>
+      <div className="copyright">
+        <span><FontAwesomeIcon icon={["fal", "copyright"]} size="1x"/> 2017 Ethan Arrowood</span>
       </div>
     </div>
   </div>
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import fontawesome from '@fortawesome/fontawesome';
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/fontawesome-free-brands';
+import { faCopyright } from '@fortawesome/fontawesome-pro-light';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-fontawesome.library.add(faFacebook, faTwitter, faInstagram);
+fontawesome.library.add(faFacebook, faTwitter, faInstagram, faCopyright);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
